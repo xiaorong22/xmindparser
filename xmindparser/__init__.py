253b4be5b652d43e0b954a01c7a3cdb6ec3b6332
xiaorong22 <130651661+xiaorong22@users.noreply.github.com>
2023-08-31 10:44:20 +0800

    Update __init__.py
    add configuration showRelationship configuration, defaultValue is False

--- xmindparser/__init__.py
+++ xmindparser/__init__.py
@@ -12,6 +12,7 @@ config = {'logName': __name__,
           'logLevel': None,
           'logFormat': '%(asctime)s %(levelname)-8s: %(message)s',
           'showTopicId': False,
+          'showRelationship': False,
           'hideEmptyValue': True}
 
 cache = {}
